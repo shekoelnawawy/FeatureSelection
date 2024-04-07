32758ddf07b2ac08927fdd3157c36e2e2142fe91
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2024-04-07 16:38:05 -0700

    update13

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sklearn.tree import DecisionTreeClassifier
 from imblearn.over_sampling import SMOTE
 from tsfresh import select_features
 from collections import Counter
+from tsfresh.feature_selection.relevance import calculate_relevance_table
 
 
 feature_set = 'Comprehensive' #'Minimal
@@ -24,24 +25,38 @@ standardize = False
 features.dropna(axis=1, inplace=True)
 # print(features)
 y = pd.Series(['High', 'High', 'High', 'High', 'High', 'Low', 'High', 'Low', 'Low', 'High', 'High', 'High'])
-# y = pd.Series([1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1])
-# y = pd.Series([1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0])
-# features_selected = select_features(features, y)
-# print(features_selected)
-# transform the dataset
-# counter = Counter(y)
-# print(counter)
+
 oversample = SMOTE(k_neighbors=2)
 features, y = oversample.fit_resample(features, y)
-# counter = Counter(y)
-# print(counter)
-
-features_selected = select_features(features, y)
-print(features_selected)
 
 
-# X_full_train, X_full_test, y_train, y_test = train_test_split(features, y, test_size=.4)
-#
-# classifier_full = DecisionTreeClassifier()
-# classifier_full.fit(X_full_train, y_train)
-# print(classification_report(y_test, classifier_full.predict(X_full_test)))
+relevance_table = calculate_relevance_table(features, y)
+relevance_table = relevance_table[relevance_table.relevant]
+relevance_table.sort_values("p_value", inplace=True)
+print(relevance_table["feature"][:11])
+
+exit(1)
+
+X_full_train, X_full_test, y_train, y_test = train_test_split(features, y, test_size=.4)
+
+classifier_full = DecisionTreeClassifier()
+classifier_full.fit(X_full_train, y_train)
+print(classification_report(y_test, classifier_full.predict(X_full_test)))
+
+column_names = features.columns
+if standardize:
+    # data = StandardScaler().fit_transform(features.iloc[:, :-1])
+    data = StandardScaler().fit_transform(features)
+else:
+    # data = np.array(features.iloc[:,:-1])
+    data = np.array(features)
+
+# vulnerability = np.array(features.iloc[:,-1])
+
+# seeds = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000]
+# for seed in seeds:
+#     clf = tree.DecisionTreeClassifier(random_state=seed)
+#     clf = clf.fit(data, y)
+#     tree.plot_tree(clf, feature_names=column_names[:-1], filled=True, class_names=['High', 'Low'], rounded = True)
+#     # plt.show()
+#     plt.savefig('./Trees/tree_'+str(seed), dpi=300)
